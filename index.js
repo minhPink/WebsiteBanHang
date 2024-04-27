efa15b0629f424868417ee75fb38b62ca020c548
unknown <kirito14042003@gmail.com>
2024-04-27 10:23:21 +0700

    thay doi

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ database.connect();
 const app = express();
 const port = process.env.PORT;
 app.use(methodOverride('_method'));
-app.set('views', './views');
+app.set('views', `${__dirname}/views`);
 app.set('view engine', 'pug');
 
-app.use(express.static('public'))
+app.use(express.static(`${__dirname}/public`));
 
 // App Locals Variable
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
